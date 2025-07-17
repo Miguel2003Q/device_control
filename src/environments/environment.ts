3a0349683928969c1907b871cbff46b331f5a146
Miguel Vasquez <vgmiguel16@gmail.com>
2025-07-17 11:35:10 -0500

    Connect to render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080' // <-- URL del backend
+  apiUrl: 'https://devicecontrolapi-latest.onrender.com' // <-- URL del backend
 };
 
 //'http://localhost:8080' // <-- URL del backend
